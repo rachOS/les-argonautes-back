92ed8fcb87222837562773db885f7cc0d81209db
rachOS <g.chamekh@e-nautia.com>
2021-06-07 11:26:10 +0200

    [TRY] to fix routing problem on remote server

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+/* router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
-});
+}); */
 /* GET home page. */
 router.get("/api", function (req, res, next) {
   res.status(200).json({ title: "REACT" });
